748773be016cb7da92c69ff84d0303451ef6b641
AgusJuarez <agushjuarez@gmail.com>
2023-01-24 17:52:26 -0300

    Se termino con los GET. Se empieza con POST.

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -6,14 +6,15 @@ app = FastAPI()
 #Entidad user
 
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
 
-users_list = [User("Agus", "Juarez", "https://moure.dev", 23),
-              User("Alejandro", "Suarez", "https://mouredev.dev", 20),
-              User("Alan", "Gomez", "https://alan.dev", 43)]
+users_list = [User(id = 1, name = "Agus", surname = "Juarez", url = "https://moure.dev", age = 23),
+              User(id = 2, name = "Alejandro", surname = "Suarez", url = "https://mouredev.dev", age = 20),
+              User(id = 3, name = "Alan", surname = "Gomez", url = "https://alan.dev", age = 43)]
 
 @app.get("/usersjson")
 async def usersjson():
@@ -24,3 +25,22 @@ async def usersjson():
 @app.get("/users") 
 async def users():
     return users_list
+
+#Por PATH
+@app.get("/user/{id}") 
+async def user(id: int):
+    return search_user(id)
+
+#Por Query
+@app.get("/user/") 
+async def user(id: int):
+    return search_user(id)
+
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"}
+
